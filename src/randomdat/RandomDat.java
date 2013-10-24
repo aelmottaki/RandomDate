707a2ddef8705cb047e57ac1d3474e63f4272c1d
aelmottaki <elmottaki@gmail.com>
2013-10-24 21:39:03 +0200

    delete nothing

--- src/randomdat/RandomDat.java
+++ src/randomdat/RandomDat.java
@@ -10,9 +10,7 @@ package randomdat;
  */
 public class RandomDat {
 
-    /**
-     * @param args the command line arguments
-     */
+
     public static void main(String[] args) {
         // TODO code application logic here
     }
