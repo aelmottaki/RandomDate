e72711f7288f523f0ad4aa939da86756f552b593
aelmottaki <elmottaki@gmail.com>
2013-10-24 21:32:13 +0200

    initialisation
    debut avec netbeans

---
+++ src/randomdat/RandomDat.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package randomdat;
+
+/**
+ *
+ * @author EL MOTTAKI
+ */
+public class RandomDat {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
