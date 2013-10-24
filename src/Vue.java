da2f96f6b6869dfeea79800239e8a8ceb7f2be0d
aelmottaki <elmottaki@gmail.com>
2013-10-24 23:34:44 +0200

    petit modification

--- src/Vue.java
+++ src/Vue.java
@@ -31,7 +31,7 @@ public class Vue extends javax.swing.JFrame {
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGap(0, 579, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
