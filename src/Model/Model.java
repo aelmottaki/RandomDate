b3c845aa0226209e91a5204207e30b952901837c
Hamada-Madl <hamada.madll@gmail.com>
2013-10-25 21:54:41 +0000

    definition des fonctions de calcule

--- src/Model/Model.java
+++ src/Model/Model.java
@@ -10,4 +10,24 @@ package Model;
  */
 public class Model {
     private int[] cmois = {7,4,3,7,5,2,7,4,1,6,3,1};
+    
+    /**
+    *
+    * @params int year
+    */
+    static int CNumber(int year)
+    {
+        int C = 0;
+        return C;
+    }
+    static int BNumber(int year)
+    {
+        int B = 0;
+        return B;
+    }  
+    static int getDayNombre(int day,int month,int year)
+    {
+        return 0;
+    }
+    
 }
