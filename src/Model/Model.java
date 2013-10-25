aafe851e106c87c8e2f41560ed64124636e0e6ed
Hamada-Madl <hamada.madll@gmail.com>
2013-10-25 22:30:18 +0000

    complétude de definition des fonctions du module

--- src/Model/Model.java
+++ src/Model/Model.java
@@ -12,7 +12,7 @@ public class Model {
     private int[] cmois = {7,4,3,7,5,2,7,4,1,6,3,1};
     
     /**
-    *
+    * calculer le nombre C
     * @params int year
     */
     static int CNumber(int year)
@@ -20,14 +20,44 @@ public class Model {
         int C = 0;
         return C;
     }
+    
+    /**
+    * calculer le nombre B
+    * @params int year
+    */
     static int BNumber(int year)
     {
         int B = 0;
         return B;
     }  
+    
+    /**
+    * caluler le jour
+    * @params int day,int month,int year
+    */
     static int getDayNombre(int day,int month,int year)
     {
+     
         return 0;
     }
+    
+    /**
+    * générer un date aléatoire 
+    * @params int minYear,int maxYear
+    */
+    static int[] generateDate(int minYear,int maxYear)
+    {
+        
+        return null;
+    }
+    
+    /**
+    * sauvgarder les resultat d'une session
+    * @params String date
+    */
+    static void archive(String date){
+        
+    }
+    
     
 }
