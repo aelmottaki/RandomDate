9adcbf8ed4d0fa2f9e1ec14d6e5d954188b8932b
Hamada-Madl <hamada.madll@gmail.com>
2013-10-26 14:09:43 +0000

    mise à jour de module

--- src/Model/Model.java
+++ src/Model/Model.java
@@ -4,6 +4,12 @@ package Model;
  *
  * @author EL MOTTAKI
  */
+import java.util.Random;
+
+enum yearType{
+   patrol,leap,normal,patrolandleap;
+}
+
 public class Model {
     private int[] cmois = {7,4,3,7,5,2,7,4,1,6,3,1};
     static int startyear = 1900;static int yearend = 2100;
@@ -57,6 +63,16 @@ public class Model {
         return isleap;
     }
     
+    private boolean isPatrolYear(int year)
+    {
+        boolean ispatrol = false;
+        int diff = year - startyear;
+        if(diff % 28 == 0)
+        {
+            ispatrol = true;
+        }
+        return ispatrol;
+    }
     
     /**
     * caluler le jour
@@ -72,10 +88,18 @@ public class Model {
     * générer un date aléatoire 
     * @params int minYear,int maxYear
     */
-    static int[] generateDate(int minYear,int maxYear)
+    static int[] generateDate(int minyear,int maxyear)
     {
-        
-        return null;
+        yearType ytype;
+        Random rnd = new Random();
+        //generer une année aléatoire
+        int year = minyear + rnd.nextInt(maxyear-minyear);
+        //generer un mois aléatoire
+        int month = rnd.nextInt(11) + 1;
+        //generer un jour aléatoire
+        int day = 0;
+        int[] date = {day,month,year};
+        return date;
     }
     
     /**
